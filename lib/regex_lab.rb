88e70654d7a7edd2117be997daa3cda5c2a48eda
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 21:03:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun.+ing/)
+  #text.scan(/\bun.+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w[a-zA-Z]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
